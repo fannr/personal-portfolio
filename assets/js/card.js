3b9b6103ae39a05e4d8cc4255ffd191e10545b7b
fannr <fatahnoer398@gmail.com>
2022-08-16 02:05:26 +0700

    add to github

---
+++ assets/js/card.js
@@ -0,0 +1,83 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const card = [
+    {
+      title: "Dasteen Blog",
+      desc: "Created a website that contains a personal landing page entitled dasteen.blog, this design I got from my friend named dastin",
+      subtitle: ["HTML", "Bootstrap 5", "Javascript"],
+      linkGithub: "https://github.com/fannr/Dasteen-Blog",
+      linkProject: "https://dasteenblog.netlify.app/",
+    },
+    {
+      title: "Navbar & Hamburger",
+      desc: "Create a design navbar & hamburger menu",
+      subtitle: ["HTML", "CSS", "Javascript"],
+      linkGithub: "https://github.com/fannr/hamburger-menu",
+      linkProject: "https://test-hamburger-menu.netlify.app/",
+    },
+    {
+      title: "Todo Blog App",
+      desc: "Create todo blog app there is a simple crud with localStorage",
+      subtitle: ["HTML", "CSS", "Javascript"],
+      linkGithub: "https://github.com/fannr/Todo-blog-app",
+      linkProject: "https://noerra-todo-blog-app.netlify.app/",
+    },
+  ];
+
+  const projectContainer = document.querySelector(".container__card");
+  (function showCard() {
+    projectContainer.innerHTML = card
+      .map((c) => {
+        return `<div animate class="project__card animate">
+      <div animate class="project__icon">
+        <i class="bi bi-folder"></i>
+
+        <div class="project__icons">
+          <a
+            href="${c.linkGithub}"
+            target="_blank"
+          >
+            <i class="bi bi-github"></i>
+          </a>
+          <a href="${c.linkProject}" target="_blank">
+            <i class="bi bi-box-arrow-up-right"></i>
+          </a>
+        </div>
+      </div>
+      <h3 class="project__title">${c.title}</h3>
+      <p class="project__desc">
+        ${c.desc}.
+      </p>
+      <div class="project__subtitle">
+      ${c.subtitle
+        .map((s) => {
+          return `<small>${s}</small>`;
+        })
+        .join("")}
+      </div>
+    </div>`;
+      })
+      .join("");
+  })();
+
+  const elSection = document.querySelectorAll("[data-scroll-banner]");
+  for (const link of elSection) {
+    navLinkIntersection.observe(link);
+  }
+
+  const elAnimate = document.querySelectorAll("[animate]");
+  const animateObserver = new IntersectionObserver(
+    (entries) => {
+      for (const entry of entries) {
+        entry.target.classList.toggle("fadeUp", entry.isIntersecting);
+        if (entry.isIntersecting) animateObserver.unobserve(entry.target);
+      }
+    },
+    {
+      threshold: 0.7,
+    }
+  );
+
+  for (const el of elAnimate) {
+    animateObserver.observe(el);
+  }
+});
---
+++ assets/js/script.js
@@ -0,0 +1,136 @@
+const nav = document.querySelector(".navbar");
+const menuBtn = document.querySelector(".menu-btn");
+const navbarNav = document.querySelector(".navbar-nav");
+const elNavlink = document.querySelectorAll(".navbar-nav .navLink");
+
+window.addEventListener("resize", function () {
+  if (this.innerWidth >= 991) {
+    menuBtn.classList.remove("active");
+    navbarNav.classList.remove("active");
+  }
+});
+
+const loading = document.querySelector(".loading");
+window.addEventListener("load", function () {
+  setTimeout(() => {
+    loading.classList.add("fadeOut");
+  }, 2000);
+
+  setTimeout(() => {
+    document.body.classList.add("active");
+  }, 2500);
+
+  setTimeout(() => {
+    loading.remove();
+  }, 3000);
+
+  const listNavbar = document.querySelectorAll("[animate-nav]");
+  setTimeout(() => {
+    for (const list of listNavbar.entries()) {
+      setTimeout(() => {
+        list[1].classList.add("fadeUp");
+      }, 100 * list[0]);
+    }
+  }, 2600);
+
+  const elHome = document.querySelectorAll("[animateHome]");
+  setTimeout(() => {
+    for (const el of elHome.entries()) {
+      setTimeout(() => {
+        el[1].classList.add("fadeDown");
+      }, 100 * el[0]);
+    }
+  }, 3300);
+});
+
+let currentScroll = 0;
+window.addEventListener("scroll", function () {
+  let scroll = this.scrollY;
+
+  if (scroll == 0) {
+    nav.classList.remove("scrollUp");
+    nav.classList.remove("scrollDown");
+  } else if (scroll >= currentScroll) {
+    menuBtn.classList.remove("active");
+    navbarNav.classList.remove("active");
+    nav.classList.add("scrollUp");
+    nav.classList.remove("scrollDown");
+  } else {
+    menuBtn.classList.remove("active");
+    navbarNav.classList.remove("active");
+    nav.classList.add("scrollDown");
+    nav.classList.remove("scrollUp");
+  }
+
+  currentScroll = scroll;
+});
+
+const navA = document.querySelectorAll("a");
+for (const a of navA) {
+  a.addEventListener("click", function (e) {
+    menuBtn.classList.remove("active");
+    navbarNav.classList.remove("active");
+
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+
+    e.preventDefault();
+  });
+}
+
+document.body.addEventListener("click", function (e) {
+  if (e.target.id != "menu" && e.target.id != "navbarNav") {
+    menuBtn.classList.remove("active");
+    navbarNav.classList.remove("active");
+  }
+});
+
+menuBtn.addEventListener("click", function () {
+  this.classList.toggle("active");
+  navbarNav.classList.toggle("active");
+});
+
+const projectIcons = document.querySelectorAll(".project__icons");
+const titleProject = document.querySelectorAll(".project__title");
+
+for (const icons of projectIcons) {
+  icons.addEventListener("mouseover", function (e) {
+    if (
+      e.target.className == "bi bi-github" ||
+      e.target.className == "bi bi-box-arrow-up-right"
+    ) {
+      for (const title of titleProject) {
+        title.classList.add("removeHover");
+      }
+    }
+  });
+
+  icons.addEventListener("mouseout", function (e) {
+    if (
+      e.target.className == "bi bi-github" ||
+      e.target.className == "bi bi-box-arrow-up-right"
+    ) {
+      for (const title of titleProject) {
+        title.classList.remove("removeHover");
+      }
+    }
+  });
+}
+
+const navLinkIntersection = new IntersectionObserver(
+  (entries) => {
+    for (const e of entries) {
+      if (e.isIntersecting) {
+        for (const elLink of elNavlink) {
+          elLink.classList.remove("active");
+
+          if (e.target.id === elLink.dataset.navLink) {
+            elLink.classList.add("active");
+          }
+        }
+      }
+    }
+  },
+  { threshold: 0.85 }
+);
